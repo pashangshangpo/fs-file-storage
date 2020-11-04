c1f3a6c974ca2f98d4e84120d1b62cae9e46b44e
pashangshangpo <pashangshangpo@gmail.com>
2020-11-04 22:03:50 +0800

    chore: 完成fs-file-storage1.0.0版本

--- demo/test.js
+++ demo/test.js
@@ -1,4 +1,76 @@
 const Storage = require('../dist/fs-file-storage')
-const storage = Storage('./blog')
+const storage = Storage('blog')
 
-console.log(storage)
+// storage.add(
+//   {
+//     name: 'Hello World'
+//   },
+//   {
+//     content: 'content.....'
+//   }
+// )
+
+// storage.add(
+//   {
+//     name: 'Hello World 3'
+//   }
+// )
+
+// storage.getList().then(list => {
+//   // storage.get(list[1]._id).then(console.log)
+//   storage.get({
+//     name: 'Hello World 3'
+//   }).then(console.log)
+// })
+
+// storage.search(item => item.name.includes('Hello World 3') || item.name.includes('Hello World 2')).then(console.log)
+
+// storage.remove('1759314c3c8d65f49d').then(console.log)
+// storage.remove({
+//   name: 'Hello World 3'
+// }).then(console.log)
+
+// storage.set(
+//   '17593784a6fa29b0c5',
+//   {
+//     content: 'hello11',
+//     content2: 'world11'
+//   },
+//   // {
+//   //   infoJson: {
+//   //     content3: '555'
+//   //   },
+//   //   replace: true
+//   // }
+// )
+
+// storage.insert(
+//   0,
+//   {
+//     name: '张三'
+//   }
+// )
+
+// storage.insert(
+//   0,
+//   {
+//     name: '李四'
+//   },
+//   {
+//     content: 'hhhh'
+//   }
+// )
+
+// storage.insert(
+//   1,
+//   {
+//     name: '王武'
+//   }
+// )
+
+// storage.insert(
+//   '175938e57136a56bb3',
+//   {
+//     name: '666'
+//   }
+// )
--- dist/fs-file-storage.mjs
+++ dist/fs-file-storage.mjs
@@ -1,1 +1,418 @@
-import n from"fs";import"path";"undefined"!=typeof Symbol&&(Symbol.iterator||(Symbol.iterator=Symbol("Symbol.iterator")));"undefined"!=typeof Symbol&&(Symbol.asyncIterator||(Symbol.asyncIterator=Symbol("Symbol.asyncIterator")));var t=function(t){return new Promise(function(o){n.access(t,n.constants.F_OK,function(n){o(!n)})})},o=function(r){try{var e=r.split("/");return e.pop(),Promise.resolve(t(e.join("/"))).then(function(i){function c(){return new Promise(function(o){n.mkdir(r,function(){try{return Promise.resolve(t(r)).then(function(n){o(n)})}catch(n){return Promise.reject(n)}})})}var u=function(){if(!i)return Promise.resolve(o(e.join("/"))).then(function(){})}();return u&&u.then?u.then(c):c()})}catch(n){return Promise.reject(n)}};export default function(n){o(n);return{getList:function(){},search:function(){},get:function(){},add:function(n,t){},set:function(){},insert:function(){},sort:function(n){},remove:function(n){}}}
+import Fs from 'fs';
+import path, { join } from 'path';
+import Os from 'os';
+
+const _iteratorSymbol = /*#__PURE__*/ typeof Symbol !== "undefined" ? (Symbol.iterator || (Symbol.iterator = Symbol("Symbol.iterator"))) : "@@iterator";
+
+const _asyncIteratorSymbol = /*#__PURE__*/ typeof Symbol !== "undefined" ? (Symbol.asyncIterator || (Symbol.asyncIterator = Symbol("Symbol.asyncIterator"))) : "@@asyncIterator";
+
+/**
+ * 初始化目录
+ * @param {String} path 目录路径
+ */
+
+var initDir = function (path$$1) {
+  try {
+    var splitLine = Os.platform() === 'win32' ? '\\' : '/';
+    var paths = path$$1.split(splitLine);
+    paths.pop();
+    var dir = paths.join(splitLine);
+
+    var _temp2 = function () {
+      if (dir) {
+        return Promise.resolve(exists(dir)).then(function (isExists) {
+          var _temp = function () {
+            if (!isExists) {
+              return Promise.resolve(mkdir(dir)).then(function () {});
+            }
+          }();
+
+          if (_temp && _temp.then) { return _temp.then(function () {}); }
+        });
+      }
+    }();
+
+    return Promise.resolve(_temp2 && _temp2.then ? _temp2.then(function () {}) : void 0);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+};
+/**
+ * 判断文件路径是否存在
+ * @param {String} path 文件路径
+ */
+
+
+var exists = function (path$$1) {
+  return new Promise(function (resolve) {
+    Fs.access(path$$1, Fs.constants.F_OK, function (err) {
+      resolve(err ? false : true);
+    });
+  });
+};
+/**
+ * 写入JSON数据到文件
+ * @param {String} path 文件路径
+ * @param {Object} json JSON数据
+ */
+
+
+var writeJson = function (path$$1, json) {
+  try {
+    return Promise.resolve(initDir(path$$1)).then(function () {
+      return new Promise(function (resolve) {
+        Fs.writeFile(path$$1, JSON.stringify(json), function (err) {
+          resolve(err ? false : true);
+        });
+      });
+    });
+  } catch (e) {
+    return Promise.reject(e);
+  }
+};
+/**
+ * 读取Json文件数据
+ * @param {String} path 文件路径
+ */
+
+
+var readJson = function (path$$1) {
+  return new Promise(function (resolve) {
+    Fs.readFile(path$$1, function (err, data) {
+      data = data.toString() || null;
+
+      if (data) {
+        data = JSON.parse(data);
+      }
+
+      resolve(err ? null : data);
+    });
+  });
+};
+/**
+ * 创建目录
+ * @param {String} path 目录路径
+ */
+
+
+var mkdir = function (path$$1) {
+  try {
+    return Promise.resolve(initDir(path$$1)).then(function () {
+      return new Promise(function (resolve) {
+        Fs.mkdir(path$$1, function () {
+          try {
+            return Promise.resolve(exists(path$$1)).then(function (_exists) {
+              resolve(_exists);
+            });
+          } catch (e) {
+            return Promise.reject(e);
+          }
+        });
+      });
+    });
+  } catch (e) {
+    return Promise.reject(e);
+  }
+};
+/**
+ * 删除文件
+ * @param {String} path 文件路径
+ */
+
+
+var deleteFile = function (path$$1) {
+  return new Promise(function (resolve) {
+    Fs.unlink(path$$1, function (err) {
+      resolve(err ? false : true);
+    });
+  });
+};
+
+var getUUID = function () {
+  var time = Date.now().toString(16);
+  var random = Math.random().toString(16).slice(2);
+  return (time + random).slice(0, 18);
+};
+
+var index = (function (dataPath) {
+  var indexPath = path.join(dataPath, 'indexs');
+  var contentsPath = path.join(dataPath, 'contents');
+  /**
+   * 初始文件目录
+   */
+
+  var init = function () {
+    try {
+      return Promise.resolve(exists(dataPath)).then(function (_exists) {
+
+        function _temp5(_result) {
+          return Promise.resolve(exists(indexPath)).then(function (_exists2) {
+            function _temp3() {
+              return Promise.resolve(exists(contentsPath)).then(function (_exists3) {
+                var _temp = function () {
+                  if (!_exists3) {
+                    return Promise.resolve(mkdir(contentsPath)).then(function () {});
+                  }
+                }();
+
+                if (_temp && _temp.then) { return _temp.then(function () {}); }
+              });
+            }
+
+            var _temp2 = function () {
+              if (!_exists2) {
+                return Promise.resolve(writeJson(indexPath, [])).then(function () {});
+              }
+            }();
+
+            return _temp2 && _temp2.then ? _temp2.then(_temp3) : _temp3(_temp2);
+          });
+        }
+
+        var _temp4 = function () {
+          if (!_exists) {
+            return Promise.resolve(mkdir(dataPath)).then(function () {});
+          }
+        }();
+
+        return _temp4 && _temp4.then ? _temp4.then(_temp5) : _temp5(_temp4);
+      });
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+  /**
+   * 获取当前数据列表
+   * @returns {Array} [] 返回获列表数据
+   */
+
+
+  var getList = function () { return readJson(indexPath); };
+  /**
+   * 查找数据
+   * @param {Object|Function} filterJson 索引数据json对象或过滤方法
+   */
+
+
+  var search = function (filterJson) {
+    try {
+      return Promise.resolve(getList()).then(function (list) {
+        return typeof filterJson === 'function' ? list.filter(filterJson) : list.filter(function (item) {
+          for (var i = 0, list = Object.keys(filterJson); i < list.length; i += 1) {
+            var key = list[i];
+
+            if (filterJson[key] !== item[key]) {
+              return false;
+            }
+          }
+
+          return true;
+        });
+      });
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+  /**
+   * 获取单条数据
+   * @param {Object|String} filterJson 过滤对象或id
+   * @return {Object} infoJson 详情数据json对象
+   */
+
+
+  var get = function (filterJson) {
+    try {
+      if (typeof filterJson === 'string') {
+        return Promise.resolve(readJson(join(contentsPath, filterJson)));
+      }
+
+      return Promise.resolve(getList()).then(function (list) {
+        var item = list.find(function (li) {
+          for (var i = 0, list = Object.keys(filterJson); i < list.length; i += 1) {
+            var key = list[i];
+
+            if (li[key] !== filterJson[key]) {
+              return false;
+            }
+          }
+
+          return true;
+        });
+        return item ? readJson(join(contentsPath, item._id)) : null;
+      });
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+  /**
+   * 添加数据
+   * @param {Object} json 索引数据json对象
+   * @param {Object} infoJson 详情数据json对象
+   * @returns {number} 返回数组长度
+   */
+
+
+  var add = function (json, infoJson) {
+    try {
+      return Promise.resolve(init()).then(function () {
+        return Promise.resolve(getList()).then(function (list) {
+          var id = getUUID();
+          var newJson = Object.assign(json, {
+            _id: id
+          });
+          list.push(newJson);
+          return Promise.resolve(writeJson(indexPath, list)).then(function () {
+            return Promise.resolve(writeJson(join(contentsPath, id), Object.assign({}, newJson,
+              infoJson))).then(function () {
+              return list.length;
+            });
+          });
+        });
+      });
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+  /**
+   * 数据修改
+   * @param {Object|String}  filterJson 过滤对象或ID
+   * @param {Object} json 数据对象
+   * @param {Object} 可选对象 { infojson = {}, replace = false }
+   */
+
+
+  var set = function (filterJson, json, ref) {
+    if ( ref === void 0 ) ref = {};
+    var infoJson = ref.infoJson;
+    var replace = ref.replace;
+
+    try {
+      return Promise.resolve(get(filterJson)).then(function (item) {
+        return Promise.resolve(getList()).then(function (list) {
+          var id = item._id;
+          var index = list.findIndex(function (li) { return li._id === id; });
+
+          if (replace) {
+            list.splice(index, 1, Object.assign({}, json,
+              {_id: id}));
+            writeJson(indexPath, list);
+            writeJson(join(contentsPath, id), Object.assign({}, json,
+              infoJson,
+              {_id: id}));
+            return;
+          }
+
+          list.splice(index, 1, Object.assign({}, list[index],
+            json));
+          writeJson(indexPath, list);
+          writeJson(join(contentsPath, id), Object.assign({}, item,
+            json,
+            infoJson));
+        });
+      });
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+  /**
+   * 数据插入到指定位置
+   * @param {Number|String} index数据插入位置或Id
+   * @param {Object} json 数据对象
+   * @param {Object} infojson 修改主体json
+   */
+
+
+  var insert = function (index, json, infoJson) {
+    try {
+      return Promise.resolve(init()).then(function () {
+        return Promise.resolve(getList()).then(function (list) {
+          var id = getUUID();
+          var newJson = Object.assign({}, json,
+            {_id: id});
+
+          if (typeof index === 'string') {
+            index = list.findIndex(function (item) { return item._id === index; });
+          }
+
+          list.splice(index, 0, newJson);
+          writeJson(indexPath, list);
+          writeJson(join(contentsPath, id), Object.assign({}, newJson,
+            infoJson));
+          return list.length;
+        });
+      });
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+  /**
+   * 数据排序
+   * @param {Function} fun 传递一个函数，对数据进行排序
+   * @returns {Array} 排序后的数组
+   */
+
+
+  var sort = function (fun) {
+    try {
+      return Promise.resolve(getList()).then(function (list) {
+        return list.sort(fun);
+      });
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+  /**
+   * 删除数据
+   * @param {Object|String} filterKey 删除符合条件的数据
+   * @returns {Number} index 被删除的索引
+   */
+
+
+  var remove = function (filterKey) {
+    try {
+      return Promise.resolve(getList()).then(function (list) {
+        var index = -1;
+
+        if (typeof filterKey === 'string') {
+          index = list.findIndex(function (item) { return item._id === filterKey; });
+        } else {
+          index = list.findIndex(function (item) {
+            for (var i = 0, list = Object.keys(filterKey); i < list.length; i += 1) {
+              var key = list[i];
+
+              if (filterKey[key] !== item[key]) {
+                return false;
+              }
+            }
+
+            return true;
+          });
+        }
+
+        if (index > -1) {
+          var id = list[index]._id;
+          list.splice(index, 1);
+          writeJson(indexPath, list);
+          deleteFile(join(contentsPath, id));
+        }
+
+        return index;
+      });
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+
+  return {
+    getList: getList,
+    search: search,
+    get: get,
+    add: add,
+    set: set,
+    insert: insert,
+    sort: sort,
+    remove: remove
+  };
+});
+
+export default index;
